9d4420a5cde9e26dcf91129bb6fb39a429403c2f
combeubeu <anthony.comberousse@etu.univ-lyon1.fr>
2017-11-22 17:47:58 +0100

    base_de_donnees

--- classes/base_de_donnees.php
+++ classes/base_de_donnees.php
@@ -5,12 +5,25 @@
 		function connect(){
 			try{
 
+				$pdo = new PDO($dsn, 'root', '');
+				
+
+
 			}
 			catch (PDOException $e){
 				die("Erreur :". $e->getMessage());
 			}
 		}
 
+		function requete($row){
+			$requet="select * from jeux_video";
+			$stmt=$pdo->query($requet);
+			while($row = $stmt->fetch()){
+				echo $row[0];
+			}
+
+			return $row;
+		}
 
 
 	}
